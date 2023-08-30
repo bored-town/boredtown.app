29873a93fe5056eda57bf9fb86b3e7640e188d53
diewland <diewland@gmail.com>
2023-08-31 01:00:09 +0700

    revise script

--- test/run_app_v2_gas11.js
+++ test/run_app_v2_gas11.js
@@ -1,10 +1,9 @@
-let app    = '../app_v2_gas11.js';
-let config = './config.js?t=' + +(new Date()); // no cache
-
 function load_script(src, callback=null) {
   var script = document.createElement('script');
   script.src = src;
   if (callback) script.onload = callback;
   document.body.appendChild(script);
 }
-load_script(config, _  => { load_script(app) }); // load config -> app
+load_script('./config.js?t=' + +(new Date()), _  => { // 1. load config (no cache)
+  load_script('../app_v2_gas11.js');                  // 2. load app
+});
