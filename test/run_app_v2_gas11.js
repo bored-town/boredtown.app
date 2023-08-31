4e01cc6ae838feb87c5e91766d0d07578c20985d
diewland <diewland@gmail.com>
2023-08-31 10:28:00 +0700

    split abi from config to no-cache only real config

--- test/run_app_v2_gas11.js
+++ test/run_app_v2_gas11.js
@@ -5,5 +5,7 @@ function load_script(src, callback=null) {
   document.body.appendChild(script);
 }
 load_script('./config.js?t=' + +(new Date()), _  => { // 1. load config (no cache)
-  load_script('../app_v2_gas11.js');                  // 2. load app
+  load_script('../../launch/abi_v2.js', _ => {        // 2. load abi
+    load_script('../app_v2_gas11.js');                // 3. load app
+  });
 });
