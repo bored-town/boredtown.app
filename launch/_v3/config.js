8eee90b48b6cf74f1958fc873e913583afb37da0
diewland <diewland@gmail.com>
2023-09-09 09:49:34 +0700

    update v3 config to v3 contract

--- launch/_v3/config.js
+++ launch/_v3/config.js
@@ -1,13 +1,13 @@
 // chain
-const CHAIN_NAME      = "Base Mainnet";
-const CHAIN_RPC       = "https://mainnet.base.org";
-const CHAIN_ID        = 8453;
+const CHAIN_NAME      = "Sepolia Testnet";
+const CHAIN_RPC       = "https://eth-sepolia.g.alchemy.com/v2/ImOC9kCyGPgZDj0dEAAyTc9v5_tGO1Z7";
+const CHAIN_ID        = 11155111;
 const CHAIN_SYMBOL    = "ETH";
-const CHAIN_EXPLORER  = "https://basescan.org";
+const CHAIN_EXPLORER  = "https://sepolia.etherscan.io";
 
 // contract
 const MAX_SUPPLY      = 10_000;
-const MINT_PER_WALLET = 3;
+const MINT_PER_WALLET = 5;
 const WHITELIST_SRC   = null;
-const MINT_SECRET     = "ILOVEMINTDOTFUN";
-const CONTRACT_ADDR   = "0x9adaE68E13da61543c6Ef4695617DAa03f07a05d";
+const MINT_SECRET     = "ILOVEMINTDOTFUN"; // TODO change to merkle tree
+const CONTRACT_ADDR   = "0xd6C13D9A50Fc4e0fC19e584796e36Ae2Fc6ec6C2";
