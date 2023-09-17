5e94ee4723d7edef8e6379379098cabf4bf4dd77
diewland <diewland@gmail.com>
2023-09-17 23:23:17 +0700

    add [Tripster x Wally on Base]

---
+++ launch/tripsterwally-base/config.js
@@ -0,0 +1,20 @@
+// chain
+const CHAIN_NAME      = "Base Mainnet";
+const CHAIN_RPC       = "https://mainnet.base.org";
+const CHAIN_ID        = 8453;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://basescan.org";
+
+// contract
+const MAX_SUPPLY      = 3_333;
+const MINT_PER_WALLET = 3;
+const CONTRACT_ADDR   = "0x3E81538278D6f43B5eA0BDF1728530A64eBeE4e1";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = "https://diewland.github.io/proof-cdn/wally/";
+
+// twitter
+const TWEET_TEXT      = `I just minted Tripster x Wally from @Tripster_live via @TheBoredStudio!
+
+Mint yours at https://boredtown.app/launch/tripsterwally-base and unlock bonus Lucky Box and $TRIP airdrop at https://galxe.com/tripstertravelpass. Don't miss out!`;
