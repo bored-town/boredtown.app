14a12a2eb7bac028e657910d546decae3dce79d3
diewland <diewland@gmail.com>
2023-09-19 22:48:56 +0700

    add [Tripster x Wally on Op]

---
+++ launch/tripsterwally-op/config.js
@@ -0,0 +1,22 @@
+// chain
+const CHAIN_NAME      = "OP Mainnet";
+const CHAIN_RPC       = "https://mainnet.optimism.io";
+const CHAIN_ID        = 10;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = 3_333;
+const MINT_PER_WALLET = 3;
+const CONTRACT_ADDR   = "0x3c8ac104dcBf03AE12C9Ac80aa830e1b39609e97";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = "https://diewland.github.io/proof-cdn/wally/";
+
+// twitter
+const TWEET_TEXT      = `I just minted Tripster x Wally from @Tripster_live via @TheBoredStudio! 
+
+Mint yours and unlock a bonus Lucky Box and $TRIP airdrop at https://galxe.com/tripstertravelpass
+
+https://boredtown.app/launch/tripsterwally-op/`;
