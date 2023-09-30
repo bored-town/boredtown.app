df2dd0f17bd97f0efac01235df13fba80bfb7922
diewland <diewland@gmail.com>
2023-09-30 20:31:43 +0700

    prevent cache from app_v2.js

--- launch/app_v2_bundle.js
+++ launch/app_v2_bundle.js
@@ -6,6 +6,6 @@ function load_script(src, callback=null) {
 }
 load_script('./config.js?t=' + +(new Date()), _  => { // 1. load config (no cache)
   load_script('../abi_v2.js', _ => {                  // 2. load abi
-    load_script('../app_v2.js');                      // 3. load app
+    load_script('../app_v2.js?t=20230930');           // 3. load app
   });
 });
