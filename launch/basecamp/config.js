c9c3f161e8d7279c2832498f139a68cea5526ae5
diewland <diewland@gmail.com>
2023-09-30 19:17:38 +0700

    add basecamp WL

--- launch/basecamp/config.js
+++ launch/basecamp/config.js
@@ -8,6 +8,6 @@ const CHAIN_EXPLORER  = "https://basescan.org";
 // contract
 const MAX_SUPPLY      = 10_000;
 const MINT_PER_WALLET = 3;
-const WHITELIST_SRC   = null;
+const WHITELIST_SRC   = "https://diewland.github.io/proof-cdn/v2/basecamp.csv"; // null = public
 const MINT_SECRET     = "ILOVEMINTDOTFUN";
 const CONTRACT_ADDR   = "0x9adaE68E13da61543c6Ef4695617DAa03f07a05d";
