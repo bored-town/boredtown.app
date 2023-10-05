b2122b0f0b67d996d6216cfd3dcfa1b155adf16a
diewland <diewland@gmail.com>
2023-10-06 00:22:08 +0700

    test mantle: PASS

--- launch/_v3/config.js
+++ launch/_v3/config.js
@@ -1,14 +1,14 @@
 // chain
-const CHAIN_NAME      = "Sepolia Testnet";
-const CHAIN_RPC       = "https://eth-sepolia.g.alchemy.com/v2/ImOC9kCyGPgZDj0dEAAyTc9v5_tGO1Z7";
-const CHAIN_ID        = 11155111;
-const CHAIN_SYMBOL    = "ETH";
-const CHAIN_EXPLORER  = "https://sepolia.etherscan.io";
+const CHAIN_NAME      = "Mantle Testnet";
+const CHAIN_RPC       = "https://rpc.testnet.mantle.xyz";
+const CHAIN_ID        = 5001;
+const CHAIN_SYMBOL    = "MNT";
+const CHAIN_EXPLORER  = "https://explorer.testnet.mantle.xyz";
 
 // contract
 const MAX_SUPPLY      = 10_000;
 const MINT_PER_WALLET = 5;
-const CONTRACT_ADDR   = "0x1237120B6F81c52baef46Bc491bFd0267c28b17b";
+const CONTRACT_ADDR   = "0x824BaDD46A33734a8E0eBEa6afe2aB3fb8d99708";
 const MINT_GAS_RATE   = 1;
 
 // whitelist
