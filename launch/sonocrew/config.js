7b07aa2b7c3633a825a4452b9223615a3bfc6cb5
diewland <diewland@gmail.com>
2023-10-07 14:36:21 +0700

    add sonocrew (no WL yet)

---
+++ launch/sonocrew/config.js
@@ -0,0 +1,23 @@
+// chain
+const CHAIN_NAME      = "Base Mainnet";
+const CHAIN_RPC       = "https://mainnet.base.org";
+const CHAIN_ID        = 8453;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://basescan.org";
+
+// contract
+const MAX_SUPPLY      = 1_111;
+const MINT_PER_WALLET = 1;
+const CONTRACT_ADDR   = "0x5dD89771B6589228dBFaa8ecFc5Bdc2e4652E78f";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = null; // TODO "https://diewland.github.io/proof-cdn/sonocrew/";
+
+// twitter
+const TWEET_TEXT      = `I just minted Sono Crew from @sonoson12 via @TheBoredStudio ! 
+
+Mint yours and unlock a Lucky Box at https://nft.tripster.live/
+
+Minting link below 👇
+https://boredtown.app/launch/sonocrew/`;
