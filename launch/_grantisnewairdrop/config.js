3a9f27ad28458b624541802e0c5ea6a8f49dada0
diewland <diewland@gmail.com>
2023-10-08 20:20:57 +0700

    setup grantisnewairdrop (test)

---
+++ launch/_grantisnewairdrop/config.js
@@ -0,0 +1,18 @@
+// chain
+const CHAIN_NAME      = "Sepolia Testnet";
+const CHAIN_RPC       = "https://eth-sepolia.g.alchemy.com/v2/ImOC9kCyGPgZDj0dEAAyTc9v5_tGO1Z7";
+const CHAIN_ID        = 11155111;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = 3_000;
+const MINT_PER_WALLET = 2;
+const CONTRACT_ADDR   = "0x88A0A11d9eA0390901F5D9dF41d839FC139Fd1E9";
+const MINT_GAS_RATE   = 1.1; // +10%
+
+// whitelist
+const PROOF_URL       = "https://diewland.github.io/proof-cdn/_v3/"; // null = public
+
+// twitter
+const TWEET_TEXT      = "I just minted #grantisnewairdrop on @boredtownnft launchpad. https://boredtown.app/launch/_grantisnewairdrop";
