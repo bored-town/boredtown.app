38eacac23fd5ca5be9dcf0e5a51b9fb5047ef762
diewland <diewland@gmail.com>
2023-10-12 22:51:09 +0700

    test v3 on scroll-sepolia

--- launch/_v3/config.js
+++ launch/_v3/config.js
@@ -1,14 +1,14 @@
 // chain
-const CHAIN_NAME      = "Mantle Testnet";
-const CHAIN_RPC       = "https://rpc.testnet.mantle.xyz";
-const CHAIN_ID        = 5001;
-const CHAIN_SYMBOL    = "MNT";
-const CHAIN_EXPLORER  = "https://explorer.testnet.mantle.xyz";
+const CHAIN_NAME      = "Scroll Sepolia";
+const CHAIN_RPC       = "https://sepolia-rpc.scroll.io";
+const CHAIN_ID        = 534351;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia-blockscout.scroll.io";
 
 // contract
 const MAX_SUPPLY      = 10_000;
 const MINT_PER_WALLET = 5;
-const CONTRACT_ADDR   = "0x824BaDD46A33734a8E0eBEa6afe2aB3fb8d99708";
+const CONTRACT_ADDR   = "0x52ada60c99B9603309F9a5bA5faC7CEB2484948c";
 const MINT_GAS_RATE   = 1;
 
 // whitelist
