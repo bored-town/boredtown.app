0219ea054b88befe1e98cd92d3164bc2e5f4a4e3
diewland <diewland@gmail.com>
2023-10-13 17:31:34 +0700

    add [Make Art, Not ...]

---
+++ launch/make-art-not/config.js
@@ -0,0 +1,21 @@
+// chain
+const CHAIN_NAME      = "Scroll Mainnet";
+const CHAIN_RPC       = "https://rpc.scroll.io";
+const CHAIN_ID        = 534352;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://blockscout.scroll.io";
+
+// contract
+const MAX_SUPPLY      = 8_888;
+const MINT_PER_WALLET = 2;
+const CONTRACT_ADDR   = "0x90Fb81cA2FEC713C9c6b4b2694eDed668b85d5Ed";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = `I just minted “Make Art, Not ...” from @BoredTownNFT
+
+Join the movement to spread love and peace by minting yours from the link below 👇
+https://boredtown.app/launch/make-art-not/`;
