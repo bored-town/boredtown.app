595894bba8ed03274382427b08aba1350b9d2c67
diewland <diewland@gmail.com>
2023-10-16 23:32:09 +0700

    add claim page (WIP)

---
+++ claim/config.js
@@ -0,0 +1,16 @@
+// chain
+const CHAIN_NAME      = "OPGoerli Testnet";
+const CHAIN_RPC       = "https://goerli.optimism.io";
+const CHAIN_ID        = 420;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://goerli-optimism.etherscan.io";
+
+// contract
+const CONTRACT_ADDR   = "0x11Ec7F09C943372ca3290FD411fce2Ba60A15dB1";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = `*** claim success message (multi-lines support) ***`;
