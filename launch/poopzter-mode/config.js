a3a2203093741d28d67f4c9b5d495ae8d0e9ca78
diewland <diewland@gmail.com>
2023-11-20 00:29:47 +0700

    add poopzter-mode

---
+++ launch/poopzter-mode/config.js
@@ -0,0 +1,19 @@
+// chain
+const CHAIN_NAME      = "Mode Mainnet";
+const CHAIN_RPC       = "https://mainnet.mode.network/";
+const CHAIN_ID        = 34443;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://explorer.mode.network/";
+
+// contract
+const MAX_SUPPLY      = 8_888;
+const MINT_PER_WALLET = 2;
+const CONTRACT_ADDR   = "0xc14F462E63f36D9bE62b2Fbb7595E844d3bc18Ee";
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = `I just minted "Poopzter" from @BoredTownNFT 👇
+https://boredtown.app/launch/poopzter-mode/`;
