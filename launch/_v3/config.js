72a921491c91cdba05eb57d2ebcf3cad0d67b080
diewland <diewland@gmail.com>
2023-11-30 17:02:03 +0700

    update _v3 to mode-testnet

--- launch/_v3/config.js
+++ launch/_v3/config.js
@@ -1,18 +1,18 @@
 // chain
-const CHAIN_NAME      = "Manta Pacific Testnet";
-const CHAIN_RPC       = "https://manta-testnet.calderachain.xyz/http";
-const CHAIN_ID        = 3441005;
-const CHAIN_SYMBOL    = "MANTA";
-const CHAIN_EXPLORER  = "https://manta-testnet.calderaexplorer.xyz";
+const CHAIN_NAME      = "Mode Testnet";
+const CHAIN_RPC       = "https://sepolia.mode.network";
+const CHAIN_ID        = 919;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia.explorer.mode.network";
 
 // contract
-const MAX_SUPPLY      = 10_000;
-const MINT_PER_WALLET = 5;
-const CONTRACT_ADDR   = "0x608f0BC0a62d0bb42B22bAd860e90677CaE12a12";
+const MAX_SUPPLY      = 0; // open edition
+const MINT_PER_WALLET = 1;
+const CONTRACT_ADDR   = "0x90Fb81cA2FEC713C9c6b4b2694eDed668b85d5Ed";
 const MINT_GAS_RATE   = 1;
 
 // whitelist
-const PROOF_URL       = "https://diewland.github.io/proof-cdn/_v3/"; // null = public
+const PROOF_URL       = null;
 
 // twitter
 const TWEET_TEXT      = "I just minted #grantisnewairdrop on @boredtownnft launchpad. https://boredtown.app/test/grant-is-new-airdrop";
