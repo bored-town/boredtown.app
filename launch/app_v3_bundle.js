f0c2364bb37cc3d5854650e5925b8436a8295217
diewland <diewland@gmail.com>
2023-11-30 18:12:59 +0700

    clean js cache (r4)

--- launch/app_v3_bundle.js
+++ launch/app_v3_bundle.js
@@ -6,6 +6,6 @@ function load_script(src, callback=null) {
 }
 load_script('./config.js?t=' + +(new Date()), _  => { // 1. load config (no cache)
   load_script('../abi_v3.js', _ => {                  // 2. load abi
-    load_script('../app_v3.js?r=3');                  // 3. load app
+    load_script('../app_v3.js?r=4');                  // 3. load app
   });
 });
