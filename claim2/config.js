e1538de20e9f6d713ad17782498636624472bd04
diewland <diewland@gmail.com>
2023-12-23 15:58:55 +0700

    fork claim2 from claim

---
+++ claim2/config.js
@@ -0,0 +1,14 @@
+// chain
+const CHAIN_NAME      = "OP Mainnet";
+const CHAIN_RPC       = "https://mainnet.optimism.io";
+const CHAIN_ID        = 10;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
+
+// contract
+const CONTRACT_ADDR   = "0x5B1178581E6ADfa87f5Fd088C185Ec33f006BF2D";
+const TOKEN_NAME      = "OP";
+const MINT_GAS_RATE   = 1;
+
+// twitter
+const TWEET_TEXT      = `Just claimed my $OP rewards from voting in the Bored Town x Optimism Art Contest! 🚀🎨 Keep an eye out for upcoming minting and trading events from @BoredTownNFT, featuring even more exciting rewards!`;
---
+++ claim2/snapshot.js
@@ -0,0 +1,38 @@
+const SNAPSHOT_URL = 'https://diewland.github.io/proof-cdn/claim/btac23voters.csv';
+const TOKEN_PER_VOTE = 2.5;
+
+function chunk_arr(array, chunk_size) {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunk_size) {
+        chunks.push(array.slice(i, i + chunk_size));
+    }
+    return chunks;
+}
+async function load_snapshot(chunk_index=null, chunk_size=1000) {
+  let url = SNAPSHOT_URL + `?t=${+(new Date())}`;
+  let data = await $.get(url);
+  data = data.split('\n').filter(r => r).map(r => r.trim().split(','));
+  if (chunk_index != null) {
+    data = chunk_arr(data, chunk_size)[chunk_index];
+  }
+  console.log('snapshot data');
+  console.log(data);
+
+  let addrs = [];
+  let amounts = [];
+  data.forEach(r => {
+    let vote = parseInt(r[7]);
+    let amount = vote * TOKEN_PER_VOTE;
+    addrs.push(r[0]);
+    amounts.push(float2raw(amount));
+  });
+
+  console.log('addresses and amounts');
+  console.log(addrs);
+  console.log(amounts);
+
+  if (contract !== null) {
+    console.log('update contract recipients..');
+    contract.getFunction('setRecipients').send(addrs, amounts);
+  }
+}
