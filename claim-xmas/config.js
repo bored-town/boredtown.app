8fc2748c98b396b6f17a9ef5338c2c71fac141ca
diewland <diewland@gmail.com>
2023-12-24 00:51:37 +0700

    update claim xmas info

--- claim-xmas/config.js
+++ claim-xmas/config.js
@@ -6,9 +6,12 @@ const CHAIN_SYMBOL    = "ETH";
 const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
 
 // contract
-const CONTRACT_ADDR   = "0x5B1178581E6ADfa87f5Fd088C185Ec33f006BF2D";
+const CONTRACT_ADDR   = "0x5B1178581E6ADfa87f5Fd088C185Ec33f006BF2D"; // TODO
 const TOKEN_NAME      = "OP";
 const MINT_GAS_RATE   = 1;
 
 // twitter
-const TWEET_TEXT      = `Just claimed my $OP rewards from voting in the Bored Town x Optimism Art Contest! 🚀🎨 Keep an eye out for upcoming minting and trading events from @BoredTownNFT, featuring even more exciting rewards!`;
+const TWEET_TEXT      = `Ho ho ho! Just snagged my $OP rewards from the @BoredTownNFT minting & trading events! 🎄🔴 👹
+
+Merry Xmas fam, enjoy the festive season and happy $OP claiming!
+https://boredtown.app/claim-xmas`;
