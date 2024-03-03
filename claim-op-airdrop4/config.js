efce7804d57b0282eceaa48b4232acb273002086
diewland <diewland@gmail.com>
2024-03-04 01:52:13 +0700

    add claim-op-airdrop4 page

---
+++ claim-op-airdrop4/config.js
@@ -0,0 +1,14 @@
+// chain
+const CHAIN_NAME      = "OP Mainnet";
+const CHAIN_RPC       = "https://mainnet.optimism.io";
+const CHAIN_ID        = 10;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
+
+// contract
+const CONTRACT_ADDR   = "0x28e43Bb3eE202E1dA587c88B0a8398309e8D4c2D";
+const TOKEN_NAME      = "OP";
+const MINT_GAS_RATE   = 1;
+
+// twitter
+const TWEET_TEXT      = `Booyah! Just claimed my share of the $OP airdrop from @boredtownnft !  Feeling lucky to be a part of this awesome community. Join us on Discord https://discord.gg/boredtownofficial for upcoming events!`;
