1c6b2abde94d66feee7d40ee355b78ce982853ac
diewland <diewland@gmail.com>
2024-03-11 00:10:48 +0700

    add blobz-base/zora/mode

---
+++ launch/blobz-mode/config.js
@@ -0,0 +1,20 @@
+// chain *** TODO ***
+const CHAIN_NAME      = "Sepolia Testnet";
+const CHAIN_RPC       = "https://eth-sepolia.g.alchemy.com/v2/ImOC9kCyGPgZDj0dEAAyTc9v5_tGO1Z7";
+const CHAIN_ID        = 11155111;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = 0; // open edition
+const MINT_PER_WALLET = 5;
+const CONTRACT_ADDR   = "0x7e84db529eE3fe1Cc0Ac1a8757B66261358785d0"; // *** TODO ***
+const MINT_GAS_RATE   = 1;
+
+// whitelist
+const PROOF_URL       = null; // *** TODO ***
+
+// twitter
+const TWEET_TEXT      = `Snagged my freemint Blobz NFT from @BoredTownNFT to celebrate the EIP-4844 'Dencun'! Gas fees were minimal thanks to the upgrade.
+
+Mint yours at https://boredtown.app/launch/blobz-mode`;
