cbba2a98019da46ffb63b4963b68abe2f2f80ced
diewland <diewland@gmail.com>
2024-04-13 22:40:27 +0700

    add claim jk

---
+++ claim/jk/config.js
@@ -0,0 +1,17 @@
+// chain
+const CHAIN_NAME      = "BNB Smart Chain Mainnet";
+const CHAIN_RPC       = "https://bsc.nodereal.io";
+const CHAIN_ID        = 56;
+const CHAIN_SYMBOL    = "BNB";
+const CHAIN_EXPLORER  = "https://bscscan.com";
+
+// contract
+const CONTRACT_ADDR   = "0xf2A99074B0596d8F4a8aEDe56876531f9E84a35c";
+const TOKEN_NAME      = "JK";
+const MINT_GAS_RATE   = 1;
+
+// twitter
+const TWEET_TEXT      = `*** TODO ***`; // TODO
+
+// snapshot
+const SNAPSHOT_URL    = 'https://bored-town.github.io/cdn/blobz-mint/base.csv'; // TODO
