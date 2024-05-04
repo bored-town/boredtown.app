cf464422cbafd2a18c920e1d9de06fe5bfdf3089
diewland <diewland@gmail.com>
2024-05-05 00:40:57 +0700

    add blobz chapter 3 (iframe) mockup

---
+++ claim/blobz-chap3/config.js
@@ -0,0 +1,19 @@
+// chain
+const CHAIN_NAME      = "OP Mainnet";
+const CHAIN_RPC       = "https://mainnet.optimism.io";
+const CHAIN_ID        = 10;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
+
+// contract
+const CONTRACT_ADDR   = "0x0676F7F33bFE9d4D0AD5649f002184503D2f2c45";
+const TOKEN_NAME      = "BLOBz";
+const MINT_GAS_RATE   = 1;
+
+// twitter
+const TWEET_TEXT      = `Just grabbed my $BLOBZ from @BLOBz4844: https://boredtown.app/claim/blobz-base/ & pumped to shape its future in the BLOBz DAO!
+
+Claim yours if you hold BLOBz NFTz on Base! #BLOBz #DAO`;
+
+// snapshot
+const SNAPSHOT_URL    = 'https://bored-town.github.io/cdn/blobz-mint/base.csv';
