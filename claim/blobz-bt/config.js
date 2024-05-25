4c5b69954afb98c27dfdf36c9f8cf57473f45a25
diewland <diewland@gmail.com>
2024-05-25 13:54:09 +0700

    update info

--- claim/blobz-bt/config.js
+++ claim/blobz-bt/config.js
@@ -6,12 +6,12 @@ const CHAIN_SYMBOL    = "ETH";
 const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
 
 // contract
-const CONTRACT_ADDR   = "0xd55D38A560a4913Ab8B2d5f5aDFC23F0934C275B"; // TODO
+const CONTRACT_ADDR   = "0x0eb34Ba517DE994675e7f012dA18b1d638584483";
 const TOKEN_NAME      = "BLOBz";
 const MINT_GAS_RATE   = 1;
 
 // twitter
-const TWEET_TEXT      = `*** TODO ***`;
+const TWEET_TEXT      = `Fellow Bored Town holders, did you grab your $BLOBZ share from @BLOBz4844 ? Any @BoredTownNFT holder on EVM qualifies! Claim yours now before it's gone! https://boredtown.app/claim/blobz-bt/`;
 
 // snapshot
-const SNAPSHOT_URL    = "https://bored-town.github.io/op-airdrop4/blobz.csv";
+const SNAPSHOT_URL    = "https://bored-town.github.io/blobz-bt/master.csv";
