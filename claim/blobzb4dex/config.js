c7314965071164e53581c853ad122f8a05856211
diewland <diewland@gmail.com>
2024-06-19 22:11:16 +0700

    prepare blobzb4dex claim page

---
+++ claim/blobzb4dex/config.js
@@ -0,0 +1,17 @@
+// chain
+const CHAIN_NAME      = "OP Mainnet";
+const CHAIN_RPC       = "https://mainnet.optimism.io";
+const CHAIN_ID        = 10;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
+
+// contract
+const CONTRACT_ADDR   = "0x6c844eCAE80a8D727cced01013E2a6f6B1671B6b";
+const TOKEN_NAME      = "BLOBz";
+const MINT_GAS_RATE   = 1;
+
+// twitter
+const TWEET_TEXT      = `___TWEET___`;
+
+// snapshot
+const SNAPSHOT_URL    = "https://bored-town.github.io/blobzb4dex/master.csv";
