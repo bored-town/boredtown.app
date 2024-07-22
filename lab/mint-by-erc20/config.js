9d88081a6adc9f742bc98fc2d3316e8c4998b333
diewland <diewland@gmail.com>
2024-07-22 23:37:16 +0700

    dev dev

---
+++ lab/mint-by-erc20/config.js
@@ -0,0 +1,21 @@
+// chain
+const CHAIN_NAME      = "Sepolia Testnet";
+const CHAIN_RPC       = "https://rpc.sepolia.org";
+const CHAIN_ID        = 11155111;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = 10_000;
+const MINT_PER_WALLET = 10;
+const CONTRACT_ADDR   = "0x13d6C2FE8965A95D6875c230aa5EFcF017c2Aae4";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 1_000_000;
+const TOKEN_SYMBOL    = "BLOBZ";
+const TOKEN_ADDR      = "0x1fa3bD8E6Ac94bCE0dC7a81f488D6B45acbE8582";
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null;
