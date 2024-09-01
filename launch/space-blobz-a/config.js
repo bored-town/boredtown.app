a4792a9a339a8241efbdedc4c4e8513c0e04a5a2
diewland <diewland@gmail.com>
2024-09-01 15:09:24 +0700

    add space blobz tier S, A, B

---
+++ launch/space-blobz-a/config.js
@@ -0,0 +1,21 @@
+// chain TODO
+const CHAIN_NAME      = "OP Sepolia";
+const CHAIN_RPC       = "https://sepolia.optimism.io";
+const CHAIN_ID        = 11155420;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia-optimism.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = 484_400;
+const MINT_PER_WALLET = 0; // unlimit
+const CONTRACT_ADDR   = "0xc7a3Ce6Fd099662F9b76A6468624bd108e426d3f"; // TODO
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 1_000_000_000;
+const TOKEN_SYMBOL    = "BLOBZ";
+const TOKEN_ADDR      = "0x08b6E54072DF901e07Ef6cb7C2abBda5099CFB55"; // TODO
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null; // TODO
---
+++ launch/space-blobz-s/config.js
@@ -0,0 +1,21 @@
+// chain TODO
+const CHAIN_NAME      = "OP Sepolia";
+const CHAIN_RPC       = "https://sepolia.optimism.io";
+const CHAIN_ID        = 11155420;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia-optimism.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = 48_440;
+const MINT_PER_WALLET = 0; // unlimit
+const CONTRACT_ADDR   = "0xC1de51905C6175b0881b4234aB9901e27bF37A24"; // TODO
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 10_000_000_000;
+const TOKEN_SYMBOL    = "BLOBZ";
+const TOKEN_ADDR      = "0x08b6E54072DF901e07Ef6cb7C2abBda5099CFB55"; // TODO
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null; // TODO
