12bfa62a5fe7a805203832d80f49a5ce6aae9495
diewland <diewland@gmail.com>
2024-09-01 16:01:52 +0700

    change default tier from c to s

--- launch/space-blobz-c/config.js
+++ launch/space-blobz-c/config.js
@@ -6,11 +6,11 @@ const CHAIN_SYMBOL    = "ETH";
 const CHAIN_EXPLORER  = "https://sepolia-optimism.etherscan.io";
 
 // contract
-const MAX_SUPPLY      = 48_440;
+const MAX_SUPPLY      = 48_440_000;
 const MINT_PER_WALLET = 0; // unlimit
-const CONTRACT_ADDR   = "0xC1de51905C6175b0881b4234aB9901e27bF37A24"; // TODO
+const CONTRACT_ADDR   = "0x25712857f1e4cC2C528A09F89b9d98A9342F56AF"; // TODO
 const MINT_GAS_RATE   = 1;
-const MINT_PRICE      = 10_000_000_000;
+const MINT_PRICE      = 10_000_000;
 const TOKEN_SYMBOL    = "BLOBZ";
 const TOKEN_ADDR      = "0x08b6E54072DF901e07Ef6cb7C2abBda5099CFB55"; // TODO
 
