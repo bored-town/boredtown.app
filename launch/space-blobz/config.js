4b3111269ad93aebe41b66a784a9b9cbc29ffbb0
diewland <diewland@gmail.com>
2024-09-01 13:36:32 +0700

    add space blobz (tier c)

---
+++ launch/space-blobz/config.js
@@ -0,0 +1,21 @@
+// chain TODO
+const CHAIN_NAME      = "OP Sepolia";
+const CHAIN_RPC       = "https://sepolia.optimism.io";
+const CHAIN_ID        = 11155420;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia-optimism.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = 48_440_000;
+const MINT_PER_WALLET = 0; // unlimit
+const CONTRACT_ADDR   = "0x25712857f1e4cC2C528A09F89b9d98A9342F56AF"; // TODO
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 10_000_000;
+const TOKEN_SYMBOL    = "BLOBZ";
+const TOKEN_ADDR      = "0x08b6E54072DF901e07Ef6cb7C2abBda5099CFB55"; // TODO
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null; // TODO
