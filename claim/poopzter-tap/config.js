c73a0e536366ace9bfef6e558b4917eff83bb3df
diewland <diewland@gmail.com>
2024-09-06 16:03:19 +0700

    add poopzter tap

---
+++ claim/poopzter-tap/config.js
@@ -0,0 +1,17 @@
+// chain TODO
+const CHAIN_NAME      = "OP Sepolia";
+const CHAIN_RPC       = "https://sepolia.optimism.io";
+const CHAIN_ID        = 11155420;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://sepolia-optimistic.etherscan.io";
+
+// contract
+const CONTRACT_ADDR   = "0x1c6B779c27FC3D5e65e40b9849C30E2E43481698"; // TODO
+const CLAIM_AMOUNT    = 888;
+const TOKEN_SYMBOL    = "BLOBz";
+const TOKEN_ADDR      = "0x08b6E54072DF901e07Ef6cb7C2abBda5099CFB55"; // TODO
+const TOKEN_DECIMALS  = 18;
+const MINT_GAS_RATE   = 1;
+
+// twitter
+const TWEET_TEXT      = null;
