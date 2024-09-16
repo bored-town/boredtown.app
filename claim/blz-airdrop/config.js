dfe86179ed10278d2af729f5ecab072b2eb80752
diewland <diewland@gmail.com>
2024-09-16 23:08:13 +0700

    add claim $BLZ (new claim v4)

---
+++ claim/blz-airdrop/config.js
@@ -0,0 +1,21 @@
+// chain
+const CHAIN_NAME      = "Base Mainnet";
+const CHAIN_RPC       = "https://mainnet.base.org";
+const CHAIN_ID        = 8453;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://basescan.org";
+
+// contract
+const CONTRACT_ADDR   = "0x35753f3713464E83039be8b508620cbe107b7763";
+const TOKEN_SYMBOL    = "BLZ";
+const TOKEN_ADDR      = "0x8a526CEa5F2d080D48b88D9e1947FADf16e30494";
+const TOKEN_DECIMALS  = 18;
+const MINT_GAS_RATE   = 1;
+
+// twitter
+const TWEET_TEXT      = null; // *** TODO ***
+
+// snapshot
+const SNAPSHOT_URL    = "https://blobz-xyz.github.io/blz-space-airdrop/out.csv";
+const SKIP_HEADER     = true;
+const DATA_INDEX      = 2;
