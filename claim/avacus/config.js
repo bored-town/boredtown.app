c5bbb7cbe7c50b11b725a1cc86f7ab2da2c4a9f6
diewland <diewland@gmail.com>
2024-10-10 23:13:55 +0700

    add avacus claim site

---
+++ claim/avacus/config.js
@@ -0,0 +1,21 @@
+// chain
+const CHAIN_NAME      = "Base Mainnet";
+const CHAIN_RPC       = "https://mainnet.base.org";
+const CHAIN_ID        = 8453;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://basescan.org";
+
+// contract
+const CONTRACT_ADDR   = "0xd2779A62a941217173D987dAaE130dA470cBE49e";
+const TOKEN_SYMBOL    = "USDC";
+const TOKEN_ADDR      = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+const TOKEN_DECIMALS  = 6;
+const MINT_GAS_RATE   = 1;
+
+// twitter
+const TWEET_TEXT      = `*** TODO ***`;
+
+// snapshot
+const SNAPSHOT_URL    = "https://blobz-xyz.github.io/avacus-winners/out.csv";
+const SKIP_HEADER     = true;
+const DATA_INDEX      = 2;
