0752c415ca099e57b0d7084bf53363f96e20c744
diewland <diewland@gmail.com>
2024-12-02 20:16:04 +0700

    test mint kaia chain

--- launch/_v3/config.js
+++ launch/_v3/config.js
@@ -1,14 +1,14 @@
 // chain
-const CHAIN_NAME      = "Mode Testnet";
-const CHAIN_RPC       = "https://sepolia.mode.network";
-const CHAIN_ID        = 919;
-const CHAIN_SYMBOL    = "ETH";
-const CHAIN_EXPLORER  = "https://sepolia.explorer.mode.network";
+const CHAIN_NAME      = "Kairos Testnet";
+const CHAIN_RPC       = "https://public-en-kairos.node.kaia.io";
+const CHAIN_ID        = 1001;
+const CHAIN_SYMBOL    = "KAIA";
+const CHAIN_EXPLORER  = "https://kairos.kaiascan.io";
 
 // contract
-const MAX_SUPPLY      = 0; // open edition
-const MINT_PER_WALLET = 1;
-const CONTRACT_ADDR   = "0x90Fb81cA2FEC713C9c6b4b2694eDed668b85d5Ed";
+const MAX_SUPPLY      = 10_000;
+const MINT_PER_WALLET = 5;
+const CONTRACT_ADDR   = "0xa05a1e90c71896ba96aaf2edd7d18ace079eff5f";
 const MINT_GAS_RATE   = 1;
 
 // whitelist
