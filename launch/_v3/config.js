fe5ee04cb5c09edd7e14fb51a017aba35fb87bfd
diewland <diewland@gmail.com>
2024-12-02 23:27:25 +0700

    test metis chain

--- launch/_v3/config.js
+++ launch/_v3/config.js
@@ -1,14 +1,14 @@
 // chain
-const CHAIN_NAME      = "Kairos Testnet";
-const CHAIN_RPC       = "https://public-en-kairos.node.kaia.io";
-const CHAIN_ID        = 1001;
-const CHAIN_SYMBOL    = "KAIA";
-const CHAIN_EXPLORER  = "https://kairos.kaiascan.io";
+const CHAIN_NAME      = "Metis Testnet";
+const CHAIN_RPC       = "https://sepolia.metisdevops.link";
+const CHAIN_ID        = 59902;
+const CHAIN_SYMBOL    = "METIS";
+const CHAIN_EXPLORER  = "https://sepolia-explorer.metisdevops.link";
 
 // contract
 const MAX_SUPPLY      = 10_000;
 const MINT_PER_WALLET = 5;
-const CONTRACT_ADDR   = "0xa05a1e90c71896ba96aaf2edd7d18ace079eff5f";
+const CONTRACT_ADDR   = "0x1aD7E8c4A11023Da206F9c6a21Ae66D025b7f5FB";
 const MINT_GAS_RATE   = 1;
 
 // whitelist
