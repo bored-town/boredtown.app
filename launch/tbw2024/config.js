1e19027e2a60ee6f5764e04bad3bbd51d5461508
diewland <diewland@gmail.com>
2024-12-07 15:45:38 +0700

    launch TBW2024 (testnet)

---
+++ launch/tbw2024/config.js
@@ -0,0 +1,26 @@
+// chain
+const CHAIN_NAME      = "Kairos Testnet";
+const CHAIN_RPC       = "https://public-en-kairos.node.kaia.io";
+const CHAIN_ID        = 1001;
+const CHAIN_SYMBOL    = "KAIA";
+const CHAIN_EXPLORER  = "https://kairos.kaiascan.io";
+
+// contract
+const MAX_SUPPLY      = null;
+const MINT_PER_WALLET = 3;
+const MINT_PER_TIME   = 3;
+const CONTRACT_ADDR   = "0x59eac32505e8e9f6ec6248462c129ecbfe7eb65f";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = `🎉 I just minted my exclusive Boredtopia Taiwan Blockchain Week Souvenir NFT on Kaia Chain! 🏙️✨
+
+Proud to be part of #TaiwanBlockchainWeek2024,
+
+Mint yours now https://boredtown.app/launch/tbw2024 🚀`;
