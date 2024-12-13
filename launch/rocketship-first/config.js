484cba1a5be8a513e17d344b23b09776a1200830
diewland <diewland@gmail.com>
2024-12-13 17:58:02 +0700

    add fist class

---
+++ launch/rocketship-first/config.js
@@ -0,0 +1,24 @@
+// chain
+const CHAIN_NAME      = "Base Mainnet";
+const CHAIN_RPC       = "https://mainnet.base.org";
+const CHAIN_ID        = 8453;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://basescan.org";
+
+// contract
+const MAX_SUPPLY      = null;
+const MINT_PER_WALLET = 0; // set zero to show balance on screen
+const MINT_PER_TIME   = 5;
+const CONTRACT_ADDR   = "0xc371d572a299910994f48B10f649daE75209605f";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 100_000;
+const TOKEN_SYMBOL    = "BLZ";
+const TOKEN_ADDR      = "0x8a526CEa5F2d080D48b88D9e1947FADf16e30494";
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = `🔴 I just minted my Rocketship Ticket at @BoredtopiaApp Exclusive perks, and rewards—get yours before it’s gone!
+
+https://boredtown.app/launch/rocketship-first`;
