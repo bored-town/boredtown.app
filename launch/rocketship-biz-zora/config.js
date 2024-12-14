24f23ae1a7076bc62b408aad0e522e391fd4ad17
diewland <diewland@gmail.com>
2024-12-14 17:51:58 +0700

    add rocketship biz launch sites

---
+++ launch/rocketship-biz-zora/config.js
@@ -0,0 +1,24 @@
+// chain
+const CHAIN_NAME      = "Zora Mainnet";
+const CHAIN_RPC       = "https://rpc.zora.energy";
+const CHAIN_ID        = 7777777;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://explorer.zora.energy";
+
+// contract
+const MAX_SUPPLY      = null;
+const MINT_PER_WALLET = 5;
+const MINT_PER_TIME   = 5;
+const CONTRACT_ADDR   = "0x6AAD776eCCd74471683aCb1dAf38310Ce3b05a3cf";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = "https://bored-town.github.io/cdn/rocketship-biz/";
+
+// twitter
+const TWEET_TEXT      = `🔴 I just minted my Rocketship Ticket at @BoredtopiaApp  Free mint, exclusive perks, and rewards—get yours before it’s gone!
+
+https://boredtown.app/launch/rocketship-eco-zora`;
---
+++ launch/rocketship-biz/config.js
@@ -0,0 +1,24 @@
+// chain
+const CHAIN_NAME      = "OP Mainnet";
+const CHAIN_RPC       = "https://mainnet.optimism.io";
+const CHAIN_ID        = 10;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = null;
+const MINT_PER_WALLET = 5;
+const MINT_PER_TIME   = 5;
+const CONTRACT_ADDR   = "0xec093aC5f6e0AF3A98DE946f77d9E3ABbcEf60aC";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = "https://bored-town.github.io/cdn/rocketship-biz/";
+
+// twitter
+const TWEET_TEXT      = `🔴 I just minted my Rocketship Ticket at @BoredtopiaApp  Free mint, exclusive perks, and rewards—get yours before it’s gone!
+
+https://boredtown.app/launch/rocketship-eco`;
