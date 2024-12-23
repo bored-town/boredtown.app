68091afb7a3f838d1b18d56690e611c9caf48cd4
diewland <diewland@gmail.com>
2024-12-23 22:09:02 +0700

    add xmas collections

---
+++ launch/snow-blobz/config.js
@@ -0,0 +1,22 @@
+// chain
+const CHAIN_NAME      = "Base Mainnet";
+const CHAIN_RPC       = "https://mainnet.base.org";
+const CHAIN_ID        = 8453;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://basescan.org";
+
+// contract
+const MAX_SUPPLY      = null;
+const MINT_PER_WALLET = 0; // set zero to show balance on screen
+const MINT_PER_TIME   = 1;
+const CONTRACT_ADDR   = "0x3fB0406660afC6aF7EaCcD496b6E3030538F6A02";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 50_000;
+const TOKEN_SYMBOL    = "BLZ";
+const TOKEN_ADDR      = "0x8a526CEa5F2d080D48b88D9e1947FADf16e30494";
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = `Just minted my Snow BLOBZ! ❄️ Unlock a surprise item in the Boredtopia app. Get $BLZ on FWX https://swap.fwx.finance/ (USDC/BLZ pair) and mint yours. https://boredtown.app/launch/snow-blobz`;
---
+++ launch/xmas-egg/config.js
@@ -0,0 +1,22 @@
+// chain
+const CHAIN_NAME      = "Kaia Mainnet";
+const CHAIN_RPC       = "https://public-en.node.kaia.io";
+const CHAIN_ID        = 8217;
+const CHAIN_SYMBOL    = "KAIA";
+const CHAIN_EXPLORER  = "https://kaiascan.io";
+
+// contract
+const MAX_SUPPLY      = 3_344;
+const MINT_PER_WALLET = 1;
+const MINT_PER_TIME   = 1;
+const CONTRACT_ADDR   = "0x24429f6ecbb889a58e99a38995cdf0f48c2ed71c";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = "https://bored-town.github.io/cdn/xmas-egg/";
+
+// twitter
+const TWEET_TEXT      = `Woohoo! Snagged my Xmas Egg NFT from @Boredtopia! 🚀 Rocketship Pass holders, this festive gift is too good to miss. Claim yours now! https://boredtown.app/launch/xmas-egg 🎅`;
