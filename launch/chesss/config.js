8e0abf400fdb93a23159e18e404627e8d74d3906
diewland <diewland@gmail.com>
2025-01-22 16:09:51 +0700

    add chesss-cnx

---
+++ launch/chesss/config.js
@@ -0,0 +1,22 @@
+// chain
+const CHAIN_NAME      = "OP Mainnet";
+const CHAIN_RPC       = "https://mainnet.optimism.io";
+const CHAIN_ID        = 10;
+const CHAIN_SYMBOL    = "ETH";
+const CHAIN_EXPLORER  = "https://optimistic.etherscan.io";
+
+// contract
+const MAX_SUPPLY      = null;
+const MINT_PER_WALLET = 1;
+const MINT_PER_TIME   = 1;
+const CONTRACT_ADDR   = "0x0A53AC5A27381B2b9d4861B8F1EFC0A7508294B7";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null;
