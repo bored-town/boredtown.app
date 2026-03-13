569d254e40fa1f7465febffe69d4de3c09a16b76
diewland <diewland@gmail.com>
2026-03-13 21:16:45 +0700

    update claim blz 26/03 info

--- claim/blz/config.js
+++ claim/blz/config.js
@@ -6,7 +6,7 @@ const CHAIN_SYMBOL    = "ETH";
 const CHAIN_EXPLORER  = "https://basescan.org";
 
 // contract
-const CONTRACT_ADDR   = "0x904b5182cC8B084bDc84D13e42C75c4e569dEEaA"; // **TODO** 1/2
+const CONTRACT_ADDR   = "0x96c301aA10376E8688308aE8Fa05b3DEb759C156"; // **TODO** 1/2
 const TOKEN_SYMBOL    = "BLZ";
 const TOKEN_ADDR      = "0x8a526CEa5F2d080D48b88D9e1947FADf16e30494";
 const TOKEN_DECIMALS  = 18;
@@ -17,6 +17,6 @@ const TWEET_TEXT      = `Just claimed $BLZ for Space BLOBz NFT holders!
 Follow @BLOBz4844 for the latest updates on how to collect your $BLOBZ rewards and exchange them for $BLZ in the next round.`;
 
 // snapshot
-const SNAPSHOT_URL    = "https://blobz-xyz.github.io/blz-space-airdrop/202602.csv"; // **TODO** 2/2
+const SNAPSHOT_URL    = "https://blobz-xyz.github.io/blz-space-airdrop/202603.csv"; // **TODO** 2/2
 const SKIP_HEADER     = true;
 const DATA_INDEX      = 2;
